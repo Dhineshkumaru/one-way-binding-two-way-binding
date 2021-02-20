1130afbfd42c308023572cb2e868d682ddfea71b
Dhineshkumaru <dhineshkumarugendran@gmail.com>
2021-02-20 11:30:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# one-two-way-binding
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/one-two-way-binding)
---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  test = "one way binding";
+  addClass = "attrclass";
+  test1 = "another way interpolation<script></script>";
+
+  print(event) {
+    console.log(event);
+    this.name = event.target.value;
+  }
+}
